2105300f63b92f7d074d8e2d95ded5c636425e97
aknow2 <tmknym@aknow2.com>
2024-01-02 03:53:57 +0000

    Update Gemfile and main.rb

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
 ruby '3.2.2'
 
 gem 'octokit', '~> 8.0'
+gem 'rubocop', '~> 1.59', require: false
--- main.rb
+++ main.rb
@@ -1,94 +1,79 @@
+# frozen_string_literal: true
+
 require 'octokit'
 require 'json'
 
 token = ENV['GITHUB_TOKEN']
 pattern = ENV['PATTERN']
 
-
 class ImageLottery
-
   def initialize(path)
-    @images = get_only_jpg_files(path)
-  end
-
-  private def get_only_jpg_files(path)
-    new_jpg_images = []
-    files = Dir.entries(path).reject { |f| f== '.' || f == '..'}
-    files.each do |file|
-      full_path = File.join(path, file)
-      if File.directory?(full_path)
-        new_jpg_images += get_only_jpg_files(full_path)
-      else
-        new_jpg_images << full_path if full_path.include?(".jpg")
-      end
-    end
-    new_jpg_images
+    @images = only_jpg_files(path)
   end
 
   def hit
-    path = 'https://raw.githubusercontent.com/aknow2/LGTMLottery/main/' + @images.shuffle.first
-    return path
+    "https://raw.githubusercontent.com/aknow2/LGTMLottery/main/#{@images.sample}"
   end
 
+  private
+
+  def only_jpg_files(path)
+    Dir.glob("#{path}/**/*").select { |file| File.file?(file) && file.downcase.end_with?('.jpg') }
+  end
 end
 
 class ActionsEvent
+  def initialize(event_loader: -> { {} })
+    @event_data = event_loader.call
+  end
 
-    def initialize(event_loader: lambda {{}})
-      @event_data = event_loader.call
+  def pr_or_issue_number
+    if @event_data['issue']
+      @event_data['issue']['number']
+    elsif @event_data['pull_request']
+      @event_data['pull_request']['number']
     end
+  end
 
-    def get_pr_or_issue_number
-      if @event_data['issue']
-        @event_data['issue']['number']
-      elsif @event_data['pull_request']
-        @event_data['pull_request']['number']
-      end
-    end
+  def comment_body
+    comment = @event_data['comment']
+    comment['body'] if comment
+  end
 
-    def get_comment_body
-      comment = @event_data['comment']
-      comment['body'] if comment
-    end
-
-    def get_repository
-      repository = @event_data['repository']
-      repository['full_name'] if repository
-    end
-
+  def repository
+    repository = @event_data['repository']
+    repository['full_name'] if repository
+  end
 end
 
-def get_event_data_from_file
+def event_data_from_file
   event_path = ENV['GITHUB_EVENT_PATH']
   JSON.parse(File.read(event_path))
 end
 
 class CommentPatternValidator
-
   def initialize(pattern_str: String, action_event: ActionsEvent)
     @pattern = Regexp.new(pattern_str)
     @action_event = action_event
   end
 
   def should_post_image?
-    comment_body = @action_event.get_comment_body
-    has_number = @action_event.get_pr_or_issue_number != nil
-    comment_body && comment_body.match(@pattern) && has_number
+    comment_body = @action_event.comment_body
+    has_number = !@action_event.pr_or_issue_number.nil?
+    comment_body&.match(@pattern) && has_number
   end
-
 end
 
-if __FILE__ == $0
-  event = ActionsEvent.new(event_loader:  lambda { get_event_data_from_file })
+if __FILE__ == $PROGRAM_NAME
+  event = ActionsEvent.new(event_loader: -> { event_data_from_file })
   validator = CommentPatternValidator.new(pattern_str: pattern, action_event: event)
 
   if validator.should_post_image?
     client = Octokit::Client.new(access_token: token)
     lottery = ImageLottery.new('images')
 
-    repo = event.get_repository
-    image = lottery.hit
-    item_number = event.get_pr_or_issue_number
+    repo = event.repository
+    item_number = event.pr_or_issue_number
     client.add_comment(repo, item_number, "![image](#{lottery.hit})")
   end
 end
